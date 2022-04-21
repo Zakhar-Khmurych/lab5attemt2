bf55f30b51560dc9a9acde80e7152cad346d999e
Zakhar-Khmurych <zakhar.khmurych@gmail.com>
2022-04-21 19:42:50 +0300

    Initian commit 1

---
+++ lab5try1/BigInteger.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Linq;
+
+namespace lab5try1
+{
+    public class BigInteger
+    {
+        private bool _is_negative = false;
+        
+        private int[] _numbers;
+
+        private string _numbersString;
+        public BigInteger(string value)
+        {
+            char[] characters = value.ToCharArray();
+            int i = 0;
+            
+            _numbers = new int[value.Length];
+            
+            foreach (var symbol in characters)
+            {
+                _numbers[i] = Convert.ToByte(symbol)-48;
+                i += 1;
+            }
+        }
+
+        public override string ToString()
+        {
+            _numbersString = "";
+            if (_is_negative)
+            {
+                _numbersString += "-";
+            }
+            
+            for (int i = 0; i < _numbers.Length; i++)
+            {
+                _numbersString += Convert.ToString(_numbers[i]);
+            }
+
+            return _numbersString;
+        }
+        
+        public BigInteger Add(BigInteger another)
+        {
+
+            int maxLength = this._numbers.Length;
+            int minLength = another._numbers.Length;
+
+            int[] num1Copy;
+            int[] num2Copy;
+
+            if (this._numbers.Length < another._numbers.Length)
+            {
+                maxLength = another._numbers.Length;
+                minLength = this._numbers.Length;
+
+            }
+            
+            num1Copy = new int[maxLength];
+            num2Copy = new int[maxLength];
+            
+
+            for (int i = this._numbers.Length-1; i >=0; i--)
+            {
+                num1Copy[i+maxLength-this._numbers.Length] = this._numbers[i];
+            }
+
+            for (int i = another._numbers.Length-1; i >=0; i--)
+            {
+                num2Copy[i+maxLength-another._numbers.Length] = another._numbers[i];
+            }
+            
+            int[] sum;
+            sum = new int[maxLength+1];
+
+            for (int i = maxLength-1; i >=0; i--)
+            {
+                sum[i+1] += num1Copy[i] + num2Copy[i];
+            }
+
+            for (int i = sum.Length-1; i > -1; i--)
+            {
+                if (sum[i] >= 10)
+                {
+                    sum[i] = sum[i] % 10;
+                    sum[i-1] = sum[i-1] + 1;
+                    
+                }
+            }
+            
+            string s ="";
+
+            if (((sum[0] == 0) && (sum.Length>1)))
+            {
+                for (int i = 1; i < sum.Length; i++)
+                {
+                    s += sum[i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < sum.Length; i++)
+                {
+                    s += sum[i];
+                }
+            }
+            
+            BigInteger result = new BigInteger(s);
+            return result;
+        }
+        
+
+        public BigInteger Sub(BigInteger another)
+        {
+            bool isNegative=false;
+            int maxLength = this._numbers.Length;
+            int minLength = another._numbers.Length;
+
+            if (this._numbers.Length < another._numbers.Length)
+            {
+                maxLength = another._numbers.Length;
+                minLength = this._numbers.Length;
+                isNegative = true;
+            }
+
+            int[] num1Copy;
+            int[] num2Copy;
+
+            num1Copy = new int[maxLength];
+            num2Copy = new int[maxLength];
+
+
+            for (int i = this._numbers.Length-1; i >=0; i--)
+            {
+                num1Copy[i + maxLength - this._numbers.Length] = this._numbers[i];
+            }
+
+            for (int i = another._numbers.Length - 1; i>=0; i--)
+            {
+                num2Copy[i + maxLength - another._numbers.Length] = another._numbers[i];
+            }
+            
+            int[] diff;
+            diff = new int[maxLength];
+            string sub_result = "";
+            bool leadingZero = true;
+
+
+            if (this._numbers.Length > another._numbers.Length)
+            {
+                for (int i = 0; i < diff.Length; i++)
+                {
+                    diff[i] = num1Copy[i] - num2Copy[i];
+                    if (diff[i] < 0)
+                    {
+                        diff[i] += 10;
+                        diff[i - 1] += -1;
+                    }
+                }
+            }
+            else if (this._numbers.Length < another._numbers.Length)
+            {
+                for (int i = 0; i < diff.Length; i++)
+                {
+                    diff[i] = num2Copy[i] - num1Copy[i];
+                    if (diff[i] < 0)
+                    {
+                        diff[i] += 10;
+                        diff[i - 1] += -1;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < diff.Length; i++)
+                {
+                    diff[i] = num1Copy[i] - num2Copy[i];
+                }
+            }
+
+            int start_point = 0;
+            
+            for (int i = 0; i < diff.Length; i++)
+            {
+                if (diff[i] != 0)
+                {
+                    start_point = i;
+                    break;
+                }
+                else if(i==diff.Length-1)
+                {
+                    start_point = i;
+                    isNegative = false;
+                }
+            }
+            
+
+            for (int i = start_point; i < diff.Length; i++)
+            {
+                if (diff[i] >= 0)
+                {
+                    sub_result += diff[i];
+                }
+                else
+                {
+                    sub_result += diff[i]*-1;
+                    isNegative = true;
+                }
+            }
+
+            BigInteger result = new BigInteger(sub_result); 
+            result._is_negative = isNegative;
+            return result;
+        }
+
+
+        public BigInteger Mult(BigInteger another)
+        {
+            /*
+            int maxLength = this._numbers.Length;
+            int minLength = another._numbers.Length;
+            int[] num1Copy;
+            int[] num2Copy;
+            if (this._numbers.Length < another._numbers.Length)
+            {
+                maxLength = another._numbers.Length;
+                minLength = this._numbers.Length;
+            }
+            
+            num1Copy = new int[maxLength];
+            num2Copy = new int[maxLength];
+            */
+            
+           // BigInteger result = new BigInteger(Convert.ToString(Karatsuba(this, another)));
+           return null;
+        }
+
+        public BigInteger Karatsuba(BigInteger another)
+        {
+            BigInteger result;
+
+            int len = this._numbers.Length;
+            
+            if (len == 1) 
+            {
+                result = new BigInteger(Convert.ToString(this._numbers[0]*another._numbers[0]));
+            }
+            else
+            {
+                /*
+                int[] a = new int[len/2];
+                int[] b = new int[len/2];
+                int[] c = new int[len/2];
+                int[] d = new int[len/2];
+                
+                
+                for (int i = 0; i < len/2; i++)
+                {
+                    a[i] = x._numbers[i];
+                    c[i] = y._numbers[i];
+                }
+                for (int i = len/2; i < len-len/2; i++)
+                {
+                    b[i] = x._numbers[i];
+                    d[i] = y._numbers[i];
+                } */
+
+                BigInteger a = new BigInteger(this._numbersString.Substring(0, len/2));
+                BigInteger b = new BigInteger(this._numbersString.Substring(len/2+1));
+                BigInteger c = new BigInteger(another._numbersString.Substring(0, len/2));
+                BigInteger d = new BigInteger(another._numbersString.Substring(len/2+1));
+
+                BigInteger p0 = b.Karatsuba(d);
+                BigInteger p1 = a.Add(b).Karatsuba(c.Add(d)); 
+                BigInteger p2 = c.Karatsuba(c);
+
+                /*
+                BigInteger s2 = Karatsuba(p2, new BigInteger(Convert.ToString(Math.Pow(10, len))));
+                BigInteger s1 = Karatsuba(p1.Sub(p2).Sub(p0), new BigInteger(Convert.ToString(Math.Pow(10, len/2))));
+*/
+                BigInteger s2 = p2.Karatsuba(new BigInteger("00100"));
+                BigInteger s1 = p1.Sub(p2).Sub(p0).Karatsuba(new BigInteger("00010"));
+                
+                result = new BigInteger(Convert.ToString(s2.Add(s1).Add(p0)));
+
+                // result = new BigInteger(Convert.ToString(p2*Math.Pow(10, len)));
+                //(z2 × 10 ^ (m2 × 2)) + ((z1 - z2 - z0) × 10 ^ m2) + z0
+            }
+            return result;
+        }
+
+    }
+}
---
+++ lab5try1/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+
+namespace lab5try1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BigInteger x = new BigInteger("1234");
+            BigInteger y = new BigInteger("5678");
+
+            
+            Console.WriteLine(x.Add(y));
+            Console.WriteLine(x.Sub(y));
+            
+            Console.WriteLine(x.Karatsuba(y));
+           
+          // Console.WriteLine(x.Karatsuba(y));
+           
+           
+           // Console.WriteLine(x.ToString().Substring(0, 1));
+            //Console.WriteLine(x.ToString().Substring(1,1));
+            
+           // Console.WriteLine(x._numbersString.Substring(0, 2/2));
+           // Console.WriteLine(new BigInteger(Convert.ToString(x.Karatsuba(x, y))));
+
+            /*
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+            //https://www.youtube.com/watch?v=yWI2K4jOjFQ
+
+            //Console.WriteLine(x.Add(y));
+            //Console.WriteLine(x.Sub(y));
+
+            //x*y = a*c*10^(2*(n/2))+(a*d+b*c)*10^(n/2)+bd*10^0*(n/2)
+            //ac
+            //bd
+            //ac_plus_bd
+
+            //Console.WriteLine(5/2);
+
+            //int[] arr1 = { 1, 2, 3, 4, 5 };
+            //int[] arr2 = { 6, 7, 8, 9, 0 };
+
+            static string bit_depth(int[] number)
+            {
+                int[] length;
+                length = new int[number.Length];
+
+                string depth_res = "";
+
+                for (int i = 0; i < length.Length; i++)
+                {
+                    depth_res += number[i];
+                    depth_res += "*10^";
+                    depth_res += length.Length - i - 1;
+                    if (i < length.Length - 1)
+                    {
+                        depth_res += " + ";
+                    }
+                }
+
+                return depth_res;
+            }
+
+            // compare; is_odd; add zero; copy 1st, copy 2nd 
+
+            //int[] arr1 = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int[] arr1 = {1, 2};
+            int[] arr2 = {5, 6};
+
+            // 2*3 + 2*1 + 3*0 + 0*1
+
+            // Math.Pow(10, arr1.Length)
+            // Console.WriteLine(((arr1[0]*arr2[0])*Math.Pow(10, arr1.Length))+((arr1[0]*arr2[1]+arr2[0]*arr1[1])*Math.Pow(10, arr1.Length-1))+arr1[1]*arr2[1]);
+
+
+
+            static int Kar_func(int a, int b, int c, int d)
+            {
+                int result = 0;
+
+                int[] arr1 = { };
+                int[] arr2 = { };
+
+                arr1 = new int[2];
+                arr2 = new int[2];
+
+                arr1[0] = a;
+                arr1[1] = b;
+                arr2[0] = c;
+                arr2[1] = d;
+
+                result = Convert.ToInt32(((arr1[0] * arr2[0]) * Math.Pow(10, arr1.Length) +
+                                          ((arr1[0] * arr2[1] + arr2[0] * arr1[1]) * Math.Pow(10, arr1.Length - 1)) +
+                                          arr1[1] * arr2[1]));
+
+                return result;
+            }
+
+            static int Kar_func_add(int a, int b, int c, int d)
+            {
+                int result = 0;
+
+                int[] arr1 = { };
+                int[] arr2 = { };
+
+                arr1 = new int[2];
+                arr2 = new int[2];
+
+                arr1[0] = a;
+                arr1[1] = b;
+                arr2[0] = c;
+                arr2[1] = d;
+
+                //result = Convert.ToInt32(((arr1[0]*arr2[0])*Math.Pow(10, arr1.Length)+((arr1[0]*arr2[1]+arr2[0]*arr1[1])*Math.Pow(10, arr1.Length-1))+arr1[1]*arr2[1]));
+                result = Convert.ToInt32(arr1[0] * Math.Pow(10, arr1.Length * 2) +
+                                         (arr1[1] + arr2[0]) * Math.Pow(10, arr1.Length) + arr2[1]);
+
+                return result;
+            }
+
+/*
+            Console.WriteLine(Kar_func(1, 2, 5, 6)); // 10^length
+            Console.WriteLine(Kar_func(5, 6, 3, 4)); // sum; 10^length/2
+            Console.WriteLine(Kar_func(1, 2, 7, 8));
+            Console.WriteLine(Kar_func(3, 4, 7, 8)); // 10^0
+
+            Console.WriteLine(Kar_func_add(672, 1904, 936, 2652));
+*/
+
+/*
+            static string Kar_generic(int[] num1, int[] num2)
+            {
+                string result = "";
+ 
+                int ac = (Kar_func(num1[0], num1[1], num2[0], num2[1])); // 10^length
+                int cb = (Kar_func(num2[0], num2[1], num1[2], num1[3])); // sum; 10^length/2
+                int ad = (Kar_func(num1[0], num1[1], num2[2], num2[3]));
+                int bd = (Kar_func(num1[2], num1[3], num2[2], num2[3])); // 10^0
+
+                result = Convert.ToString(Kar_func_add(ac, cb, ad, bd));
+
+                return result;
+            }
+
+            int[] array1 = {1, 2, 3, 4};
+            int[] array2 = {5, 6, 7, 8};
+
+            string xx = Kar_generic(array1, array2);
+
+            Console.WriteLine(xx);
+
+               // .. int[] arrpke   1u\
+
+
+
+        }
+
+
+            // два -- карфунк
+            // чотрири -- карфунк і загальний
+            // вісім -- карфунк і загальний х4, потім загальний
+            // шістнадцять -- карфунк і загальний х4, потім загальний х4, потім загальний
+            // тридцять два -- карфунк і загальний х4, потім загальний х4, потім загальний х4, потім загальний
+            
+            
+            
+            
+            // 12*56; 12*78 + 34*56; 34*78; 
+            // 1*2 * 10^2
+            
+            
+
+            
+            
+            
+            //Console.WriteLine(bit_depth(arr1));
+            
+            static int[] Karatsuba(int[] num1, int[] num2)
+            {
+                int length = num1.Length;
+                int[] split_a = new int[num1.Length/2];
+                int[] split_b = new int[num1.Length-num1.Length/2];
+                int[] split_c = new int[num2.Length/2];
+                int[] split_d = new int[num2.Length-num2.Length/2];
+
+                for (int i = 0; i < num1.Length/2; i++)
+                {
+                    split_a[i] = num1[i];
+                    split_c[i] = num2[i];
+                }
+                for (int i = num1.Length/2; i < num1.Length-num1.Length/2; i++)
+                {
+                    split_b[i] = num1[i];
+                    split_d[i] = num2[i];
+                }
+                
+                int[] mult_res = new int[split_a.Length];
+                mult_res = split_a;
+                return mult_res;
+            }
+            
+           //Console.WriteLine(Karatsuba(arr1, arr1));
+           
+           */
+
+
+        }
+    }
+}
